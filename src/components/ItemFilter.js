530f2ed99748568177a2648876173118f7c2a73b
aa1as <sm13929881@gmail.com>
2024-06-29 22:30:15 +0900

    [ItemFilter.js] ItemFilter컴포넌트 추가 및 필터링기능 구현

---
+++ src/components/ItemFilter.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import style from "./ItemFilter.module.css";
+
+const ItemFilter = () => {
+  const [filterType, setFilterType] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  return (
+    <>
+      <div className={style.filterSortContainer}>
+        <div>
+          <label>유형 필터</label>
+          <select
+            value={filterType}
+            onChange={(e) => setFilterType(e.target.value)}
+          >
+            <option value="">전체</option>
+            <option value="식료품">식료품</option>
+            <option value="의류">의류</option>
+            <option value="가전제품">가전제품</option>
+          </select>
+        </div>
+        <div>
+          <label>정렬 기준</label>
+          <select
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="">선택</option>
+            <option value="price">가격</option>
+            <option value="date">구입 날짜</option>
+          </select>
+        </div>
+        <div>
+          <label>시작 기간</label>
+          <input
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>끝 기간</label>
+          <input
+            type="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ItemFilter;

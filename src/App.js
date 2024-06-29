91119758b44726b845379c5490a27c41bb1d005c
aa1as <sm13929881@gmail.com>
2024-06-29 22:30:49 +0900

    [ItemFilter.module.css] css추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import ItemList from "./components/ItemList";
+import ItemFilter from "./components/ItemFilter";
 
 const App = () => {
   return (
     <div>
       <ItemList />
+      <ItemFilter />
     </div>
   );
 };

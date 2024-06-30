ca987b7bb76d50eba6e6a8173c93ca3341b85b48
aa1as <sm13929881@gmail.com>
2024-06-30 23:59:03 +0900

    [Item] ㅑㅅ드페이지 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import ItemList from "./components/ItemList";
 import ItemFilter from "./components/ItemFilter";
+import Item from "./components/Item";
 
 const App = () => {
   return (
     <div>
       <ItemList />
       <ItemFilter />
+      <Item />
     </div>
   );
 };
